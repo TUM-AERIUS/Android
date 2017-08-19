e88555a1dc86c24bc9e2bb13d71551a1d06084b4
MichaelBlind <michael.blind@maxi-bayern.de>
2017-08-19 16:07:55 +0200

    Built App to Remote Control Aerius

--- app/src/main/java/michaelblind/aeriusapp/MainActivity.java
+++ app/src/main/java/michaelblind/aeriusapp/MainActivity.java
@@ -1,31 +1,51 @@
 package michaelblind.aeriusapp;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.view.MotionEventCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
 
-import java.net.Socket;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
+    private int vel = 0;
+    private int ste = 0;
+
+    final int[] i = {0};
 
     private static final float EDGE = 200;
     private static final int DOWN = 0;
     private static final int MOVE = 2;
 
-    static Socket socket;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setSocket();
+
+        new TransmitTask(false).execute();
+        initSendLoop();
     }
 
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
+    private void initSendLoop() {
+        final Handler handler = new Handler();
+        Timer timer = new Timer();
+
+        TimerTask task = new TimerTask() {
+            @Override public void run() {
+                handler.post(new Runnable() {
+                    public void run() {new TransmitTask(vel, ste).execute(); }
+                });
+            }
+        };
+
+        timer.schedule(task, 700, 100);
+    }
+
+    @Override public boolean onTouchEvent(MotionEvent event) {
         int action  = MotionEventCompat.getActionMasked(event);
 
         //Check if Event is Touch or Swipe
@@ -54,25 +74,24 @@ public class MainActivity extends AppCompatActivity {
         if (movement[0] <  0) sY += car.getHeight();
         if (movement[0] == 0) sY  = car.getY();
 
-
         float step = car.getHeight() * 0.225f;
 
         if (movement[0] < 0) step *= -1;
 
-        float[]   start = new float[]{sX, sY};
-        float[][] dest  = triangulateLocations(start, step, movement[1]);
+        float[]   start  = new float[]{sX, sY};
+        float[][] dest  = triangulateLocations(start, step, movement[1] * 1.5f);
 
         drawPointers(pointers, dest, movement[0]);
     }
 
     private float[][] triangulateLocations(float[] start, float step, float deg) {
         float microDeg = deg / 5.0f;
-        float[][] loc = new float[10][2];
+        float[][] loc = new float[12][2];
         loc[0] = start;
 
         deg = 0;
 
-        for (int i = 1; i < 10; i++) {
+        for (int i = 1; i < 12; i++) {
             deg += microDeg;
             double rad = Math.toRadians(deg);
 
@@ -86,8 +105,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void drawPointers(View[] p, float[][] dest, float velocity) {
-        float dec = 6.0f / (Math.abs(velocity) + 0.001f);
-        for (int i = 0; i < 10; i++) {
+        float dec = 2.0f / (Math.abs(velocity) + 0.001f);
+        for (int i = 0; i < 12; i++) {
             View v = p[i];
             float[] pos = dest[i];
 
@@ -114,6 +133,9 @@ public class MainActivity extends AppCompatActivity {
                 findViewById(R.id.pointer7),
                 findViewById(R.id.pointer8),
                 findViewById(R.id.pointer9),
+                findViewById(R.id.pointer10),
+                findViewById(R.id.pointer11),
+                findViewById(R.id.pointer12),
                 };
     }
 
@@ -124,11 +146,11 @@ public class MainActivity extends AppCompatActivity {
         float y_delta = delta(y, car.getY(), car.getHeight());
         float x_delta = delta(x, car.getX(), car.getWidth());
 
-        float velocity = absMin(y_delta, y_total) * 100f / y_total; //100f: Conversion to Percentage
-        float steering = absMin(x_delta, x_total) *  40f / x_total; // 40f: Conversion to 80° Field
+        int velocity = (int) (absMin(y_delta, y_total) *  30f / y_total); //100f: Conversion to %
+        int steering = (int) (absMin(x_delta, x_total) *  40f / x_total); // 20f: Conversion to 40°
 
-        print (velocity, steering);
-        send  (velocity, steering);
+        print (velocity, -steering);
+        send  (velocity, -steering);
 
         return new float[]{velocity, steering};
     }
@@ -148,15 +170,20 @@ public class MainActivity extends AppCompatActivity {
         return in;
     }
 
-    private void print (float v, float s) {
+    private void print (int v, int s) {
         TextView status = (TextView) findViewById(R.id.output);
-        status.setText(String.format("%.0f%%  %.0f°", v, s));
+        status.setText(v + "% " + s + "°");
     }
-    private void send  (float v, float s) {
-        if (socket == null) setSocket();
-        new TransmissionTask((int) v, (int) s, socket).execute();
+    private void send  (int v, int s) {
+        vel = v;
+        ste = s;
+        /*if (!cur(v, s)) new TransmitTask(v, s).execute();*/
     }
 
-    private void setSocket() {new SocketConnectTask().execute();}
-
+    private boolean cur(float v, float s) {
+        if ((int) v == vel && (int) s == ste) return true;
+        vel = (int) v;
+        ste = (int) s;
+        return false;
+    }
 }
